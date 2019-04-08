7d8fa6ef1ff3e023acd49197813d50cb1a15c391
angelorubin <angelorubin@gmail.com>
2019-04-07 22:30:47 -0300

    remove html-loader and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,5 @@
-const path = require("path");
-
 module.exports = {
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000
-  },
+  watch: true,
   module: {
     rules: [
       {
@@ -20,7 +8,21 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+            // options: { minimize: true }
+          }
+        ]
       }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
   }
 };
